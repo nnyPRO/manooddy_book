69c493db2934255fe5dc5003d3be7d4456847961
nnyPRO <nanmesanee.17@gmail.com>
2024-04-24 23:10:36 +0700

    Set up docker

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+        <Navbar/>
+        {children}
     </html>
   );
 }
+
+
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="">
+      <div>manoodyBook</div>
+      <div>
+        <button>
+          <img src="/public/Icon/hamburger-svgrepo-com.svg" alt="" />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
