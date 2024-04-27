ab6fef9cdf980054cecd8969705d70badf8aa094
nnyPRO <nanmesanee.17@gmail.com>
2024-04-28 02:24:30 +0700

    Create navbar

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,6 @@
+const Profile = () => {
+    return <div>Profile page</div>;
+  };
+  
+  export default Profile;
+  
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,91 @@
+"use client";
+{
+  /* <link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=FreeMono:wght@400;700&display=swap"
+/>; */
+}
+// import 'package:google_fonts/google_fonts.dart';
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
   return (
-    <nav className="">
-      <div>manoodyBook</div>
-      <div>
-        <button>
-          <img src="/public/Icon/hamburger-svgrepo-com.svg" alt="" />
-        </button>
-      </div>
-    </nav>
+    <div>
+      <nav className="w-full bg-[#FFACCA] fixed top-0 left-0 right-0 z-10">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              {/* LOGO */}
+              <Link href="/">
+                <h2 className="text-3xl text-[#712573] font-bold">
+                  manoodyBook
+                </h2>
+              </Link>
+              {/* HAMBURGER BUTTON FOR MOBILE */}
+              <div className="md:hidden">
+                <button
+                  className="p-2 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <Image
+                      src="/Icon/Close.svg"
+                      width={30}
+                      height={30}
+                      alt="logo"
+                      priority
+                      className="hover:invert-34 hover:sepia-44 hover:saturate-1070 hover:hue-rotate-253 hover:brightness-98 hover:contrast-105"
+                    />
+                  ) : (
+                    <Image
+                      src="/Icon/Hamburger.svg"
+                      alt="Hamburger menu"
+                      width={30}
+                      height={30}
+                      priority
+                      className="hover:invert-34 hover:sepia-44 hover:saturate-1070 hover:hue-rotate-253 hover:brightness-98 hover:contrast-105"
+                    />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? "p-12 md:p-0 block" : "hidden"
+              }`}
+            >
+              <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+                <li className="pb-6 text-xl text-[#712573] py-5 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-[#D996DB]  border-purple-900  md:hover:text-[#D996DB] md:hover:bg-transparent">
+                  <Link href="/" onClick={() => setNavbar(!navbar)}>
+                    Home
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-[#712573] py-5 px-6 text-center  border-b-2 md:border-b-0  hover:bg-[#D996DB]  border-purple-900  md:hover:text-[#D996DB] md:hover:bg-transparent">
+                  <Link href="/cart" onClick={() => setNavbar(!navbar)}>
+                    Cart
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-[#712573] py-5 px-6 text-center  border-b-2 md:border-b-0  hover:bg-[#D996DB]  border-purple-900  md:hover:text-[#D996DB] md:hover:bg-transparent">
+                  <Link href="/chat" onClick={() => setNavbar(!navbar)}>
+                    Chat
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-[#712573] py-5 px-6 text-center  border-b-2 md:border-b-0  hover:bg-[#D996DB]  border-purple-900  md:hover:text-[#D996DB] md:hover:bg-transparent">
+                  <Link href="/profile" onClick={() => setNavbar(!navbar)}>
+                    Profile
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
